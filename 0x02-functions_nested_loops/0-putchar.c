df5689ed3f0e852713bf4801d08553d37f8412ef
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-18 14:25:36 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,18 @@
  * Return: Always 0
  */
 
-int main(void)
+int _putchar(char c);
+
+int main()
 {
-	printf("_putchr\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
