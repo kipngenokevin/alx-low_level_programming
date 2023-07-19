44c72633e37b1cd7bdf985f0757765650ae513a5
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-19 07:21:32 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write a program that prints the alphabet
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int c;
+	char alphabet[c];
+
+	for (c = 97; c < 123; c++)
+	{
+		_putchar(alphabet[c]);
+	}
+	_putchar('\n');
+}
