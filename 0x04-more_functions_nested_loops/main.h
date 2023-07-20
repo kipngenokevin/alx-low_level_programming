05a39c14e7d7b7f73d2cb7faa225628eee45c1fc
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-20 14:39:25 -0700

    Is upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing declarations for all functions
+ */
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
