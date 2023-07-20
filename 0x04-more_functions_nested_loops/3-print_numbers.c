88336e39d7bd9582b5d43021dd8fd29c8c401b7b
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-20 15:24:45 -0700

    3 - Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 1 t0 9
+ * Returns: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
