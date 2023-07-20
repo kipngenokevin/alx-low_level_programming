39eb5b81c52eab6920a4d43f68f3717ae8895d4c
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-20 16:41:57 -0700

    shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: integer with the number of characters
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (i <= n)
+	{
+		_putchar(95);
+		i++;
+	}
+	_putchar('\n');
+}
