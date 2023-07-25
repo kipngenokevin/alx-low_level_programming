b71f0cde3888fb91299cd5b83181b04ff823c36a
kipngeno kevin <kipngenokevin254@gmail.com>
2023-07-25 14:54:15 -0700

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the ack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
