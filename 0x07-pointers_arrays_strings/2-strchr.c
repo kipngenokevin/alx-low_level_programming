bba6b794b869aea430a8650e98d26ac8725c2213
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-02 07:51:25 -0700

    find character c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - returns pointer to 'c'
+ * @s: string to check
+ * @c: char c
+ * Return: s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
