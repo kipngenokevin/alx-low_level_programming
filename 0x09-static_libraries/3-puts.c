cb334b920e786dbc3f222efb20228afe013a7f3d
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-04 19:03:03 -0700

    Task 0 - update 1

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,2 +1,17 @@
 #include "main.h"
-int _putchar(char c);
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
