937e82a0b3e179497d3890096343832e60063805
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-07 08:20:53 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the name of function
+ * @argc: count terminal input
+ * @argv: pointer to string variables
+ * Return: 0 success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	char *c = argv[0];
+
+	while (*c != '\0')
+	{
+		_putchar(*c);
+		c++;
+	}
+	return (0);
+}
