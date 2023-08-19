6fdaace947c1c836038849a58570afcece8d5183
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-19 06:51:17 -0700

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a practice section for typedef and struct
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog attributes
+ * @name: The name of the dog
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: This struct stores dog's attributes
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
