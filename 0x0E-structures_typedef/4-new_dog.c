6a9167561a0e9d7ef63dfe274955e6e5ebd5e4e8
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-19 08:19:28 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,24 +1,92 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
- * new_dog - creates a new dog
- * @name: do'g name
- * @age: dog's age
- * @owner: dog's owner
- * Return: dog_t
+ * new_dog - ...
+ * @name: new dog name
+ * @age: new dog age
+ * @owner: owner details
+ *
+ * Return: - dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *cutie_dog;
+	int name_1 = 0, own_1 = 0;
 
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog != NULL)
+	if (name != NULL && owner != NULL)
 	{
-		new_dog->name = name;
-		new_dog->age = age;
-		new_dog->owner = owner;
+		name_1 = _strlen(name) + 1;
+		own_1 = _strlen(owner) + 1;
+		cutie_dog = malloc(sizeof(dog_t));
+
+		if (cutie_dog == NULL)
+			return (NULL);
+
+		cutie_dog->name = malloc(sizeof(char) * name_1);
+
+		if (cutie_dog->name == NULL)
+		{
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->owner = malloc(sizeof(char) * own_1);
+
+		if (cutie_dog->owner == NULL)
+		{
+			free(cutie_dog->name);
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->name = _strcpy(cutie_dog->name, name);
+		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
+		cutie_dog->age = age;
 	}
-	return (new_dog);
-	free(new_dog);
+
+	return (cutie_dog);
+}
+
+
+/**
+ * _strlen - Return the length of a string
+ * @s: String to count
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+
+/**
+ * _strcpy - copy a string
+ * @dest: Destination value
+ * @src: Source value
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
 }
