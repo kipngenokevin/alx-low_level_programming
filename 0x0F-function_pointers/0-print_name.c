96370d0850fb4110b64ab9f2c1d7ce828cc8050b
kipngeno kevin <kipngenokevin254@gmail.com>
2023-08-23 10:00:39 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: char pointer to string
+ * @f: pointer function returning void
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
