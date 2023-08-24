62ea7bec44919fb555eb878a72d6b1486b6a6c0e
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-24 12:57:07 +0300

    Task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,4 +6,19 @@
  * @cmp: pointer to function
  * Return: integer position
  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
 
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i] != 0)
+					return (i);
+		}
+	}
+	return (-1);
+}
