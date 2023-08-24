c6d1264d818fb2ef7385508a4af8f1fb933e0309
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-25 00:24:00 +0300

    Task 0 single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints list elements
+ * @h: pointer to struct
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("{0] (nil)\n");
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
