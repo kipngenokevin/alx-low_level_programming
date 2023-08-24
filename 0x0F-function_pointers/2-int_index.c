0a302dd20e74f1f073b18c245f761d5f0b2f02f6
Kevin Kipngeno <kipngenokevin254@gmail.com>
2023-08-24 12:32:17 +0300

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,9 @@
+#include "function_pointers.h"
+/**
+ * int_index - checks if array element matches
+ * @array: pointer to array of int
+ * @size: array size
+ * @cmp: pointer to function
+ * Return: integer position
+ */
+
