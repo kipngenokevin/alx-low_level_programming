674fbe4e10ff71e9b603b65c594fddc64ccd807b
Kevin Kipngeno <kipngenokevin254@gmail.com>
2023-08-24 11:12:00 +0300

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given a parameter
+ * on each element
+ * @array: pointer to array
+ * @size: integer value of size
+ * @action: action to be performed
+ * Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    size_t i;
+
+   if (array != NULL && action != NULL)
+       for(i = 0; i < size; i++)
+       {
+           action(array[i]);
+       }
+}
