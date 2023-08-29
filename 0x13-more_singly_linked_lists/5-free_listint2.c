cf80b4e7c1e07251a3037d816500fbd0959dc9f2
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-29 19:16:52 +0300

    Memory allocation and freeing

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 	listint_t *next = NULL;
-	
-	if (*head != NULL)
-		current = *head;
+
+	current = *head;
 	while (current != NULL)
 	{
 		next = current->next;
