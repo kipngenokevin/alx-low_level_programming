46a4afa88c76d3104842a03af3b41a8df0abdcfb
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-29 19:19:40 +0300

    Free task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,6 +10,8 @@ void free_listint2(listint_t **head)
 	listint_t *current;
 	listint_t *next = NULL;
 
+	if (head == NULL || *head == NULL)
+		return;
 	current = *head;
 	while (current != NULL)
 	{
