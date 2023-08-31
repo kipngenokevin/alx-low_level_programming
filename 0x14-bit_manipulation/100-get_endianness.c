6388333b7d83145182fa9e3e20c5ca70d62974f0
kipngenokevin <kipngenokevin254@gmail.com>
2023-08-31 22:43:04 +0300

    5 & 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * get_endianness - check endianness.
+ * Return: 0 or -1
+ */
+int get_endianness(void)
+{
+	int che = 1;
+	char *endian = (char *)&che;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}}
